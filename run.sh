b33bf5e98d2e9abb0a69e9983a81e4b89b49da64
Usman Rizwan <urizwan05@gmail.com>
2023-06-17 21:28:40 -0400

    Create a run file

---
+++ run.sh
@@ -0,0 +1,5 @@
+scrapy crawl aquanaut -O aquanaut.json
+scrapy crawl diveguide -O diveguide.json
+scrapy crawl diventures -O diventures.json
+scrapy crawl BCDivingClubs -O BCDivingClubs.json
+scrapy crawl dive_clubs_germany -O dive_clubs_germany.json
