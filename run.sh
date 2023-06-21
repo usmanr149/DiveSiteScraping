716ea070d925be56ca5b2fca7b6c685edf904a5d
Usman Rizwan <urizwan05@gmail.com>
2023-06-21 01:11:38 -0400

    update run scrit

--- run.sh
+++ run.sh
@@ -2,4 +2,11 @@ scrapy crawl aquanaut -O aquanaut.json
 scrapy crawl diveguide -O diveguide.json
 scrapy crawl diventures -O diventures.json
 scrapy crawl BCDivingClubs -O BCDivingClubs.json
-scrapy crawl dive_clubs_germany -O dive_clubs_germany.json
+scrapy crawl dive_centers_worldwide -O dive_centers_worldwide.json
+scrapy crawl bsac -O bsac.json
+scrapy crawl diving_ie -O diving_ie.json
+scrapy crawl dive_ni -O dive_ni.json
+scrapy crawl mlssa -O mlssa.json
+scrapy crawl nabs -O nabs.json
+scrapy crawl ocascuba -O ocascuba.json
+scrapy crawl underwater -O underwater.json
